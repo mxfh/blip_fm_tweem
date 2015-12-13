7e787073ba14e34645ba182ff0ee3febe54ae20a
Max Friedrich Hartmann <mxfh@users.noreply.github.com>
2015-12-13 18:21:05 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,30 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
+import re
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+username = "diskurs"
+pages = 168 
+
+baseUrl = "http://blip.fm/" + username + "/?page="
+
+for x in range(1, pages):
+  html = scraperwiki.scrape(baseUrl + x)
+  doc = lxml.html.fromstring(html)
+  i = 0
+
+  for el in doc.cssselect("a.blipTypeIcon"):
+    song = el.attrib['title'].replace("\\'", "'")
+    song = re.sub("^search for ","",song)
+    songU = song.encode('utf8')
+    print songU
+    data = {
+        'row': i,
+        'song': song
+    }
+    scraperwiki.sqlite.save(unique_keys=['row'], data=data)
+    i+=1
